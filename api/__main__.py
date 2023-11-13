ac9449daeb3ed0f5217a93981c3860163f8d85bd
poohzaza166 <poohzaza166@gmail.com>
2023-11-13 18:13:22 +0700

    added file

--- api/__main__.py
+++ api/__main__.py
@@ -1,56 +1,15 @@
-# from fastapi import FastAPI, File, UploadFile
-# from pydantic import BaseModel
-# from typing import List
-# import sqlite3
-# import json
-#
-# app = FastAPI()
-#
-# conn = sqlite3.connect('data.db')
-# c = conn.cursor()
-#
-#
-# class Item(BaseModel):
-#     status: int
-#     data: dict
-#     api_key: str
-#
-#
-# @app.get('/items/', response_model=List[Item])
-# def get_items():
-#     c.execute('SELECT * FROM items')
-#     items = c.fetchall()
-#     return [Item(**item) for item in items]
-#
-#
-# @app.post('/items/', response_model=Item)
-# def create_item(item: Item):
-#     c.execute('INSERT INTO items VALUES (:id, :name, :description, :price)', item.dict())
-#     conn.commit()
-#     return item
-#
-#
-# @app.post('/upload/')
-# async def upload_file(file: UploadFile = File(...)):
-#     contents = await file.read()
-#     save_path = f'uploads/{file.filename}'
-#     with open(save_path, 'wb') as f:
-#         f.write(contents)
-#     return {'filename': file.filename}
-#
-#
-# @app.get('/info')
-# def get_info():
-#     return json.dumps({'date': '2023-09-19'})
+from fastapi import FastAPI
 
-from .chatbot import Chatbot
-from plugins.greetings_plugin import GreetingsPlugin
+app = FastAPI()
 
-bot = Chatbot()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-bot.process("Hello world!")
-# Prints "Hello there!"
-bot.process("joke")
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
 
-bot.process("This text won't match")
-# No output
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
