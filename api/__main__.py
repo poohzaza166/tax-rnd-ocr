ab3a8630f58019e746377545c7c04c7f5bb6794d
poohzaza166 <poohzaza166@gmail.com>
2023-11-18 06:19:57 +0700

    inital implementation of income/expense tracking

--- api/__main__.py
+++ api/__main__.py
@@ -1,14 +1,77 @@
 from fastapi import FastAPI
+from .orm import Session, User, Income, Expense
+
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# create a route to log income
+@app.post("/users/{user_id}/income")
+def log_income(user_id: int, amount: float, item: str):
+    # create a session
+    session = Session()
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+    # get the user by ID
+    user = session.query(User).filter_by(id=user_id).first()
+
+    # create a new income instance
+    income = Income(amount=amount, item=item, user=user)
+
+    # add the income to the session
+    session.add(income)
+
+    # commit the transaction
+    session.commit()
+
+    # close the session
+    session.close()
+
+    # return a success message
+    return {"message": "Income logged successfully"}
+
+# create a route to log expenses
+@app.post("/users/{user_id}/expenses")
+def log_expense(user_id: int, amount: float, item: str):
+    # create a session
+    session = Session()
+
+    # get the user by ID
+    user = session.query(User).filter_by(id=user_id).first()
+
+    # create a new expense instance
+    expense = Expense(amount=amount, item=item, user=user)
+
+    # add the expense to the session
+    session.add(expense)
+
+    # commit the transaction
+    session.commit()
+
+    # close the session
+    session.close()
+
+    # return a success message
+    return {"message": "Expense logged successfully"}
+
+# create a route to get user income and expenses
+@app.get("/users/{user_id}/transactions")
+def get_transactions(user_id: int):
+    # create a session
+    session = Session()
+
+    # get the user by ID
+    user = session.query(User).filter_by(id=user_id).first()
+
+    # get the user's income and expenses
+    income = [{'amount': i.amount, 'item': i.item} for i in user.income]
+    expenses = [{'amount': e.amount, 'item': e.item} for e in user.expenses]
+
+    # close the session
+    session.close()
+
+    # return the income and expenses
+    return {"income": income, "expenses": expenses}
+
+
 
 if __name__ == "__main__":
     import uvicorn
---
+++ api/orm.py
@@ -0,0 +1,92 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+# create the database engine
+engine = create_engine('sqlite:///example.db', echo=True)
+
+# create a session factory
+Session = sessionmaker(bind=engine)
+
+# create a base class for our ORM models
+Base = declarative_base()
+
+# define a model class for users
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    income = relationship('Income', back_populates='user')
+    expenses = relationship('Expense', back_populates='user')
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# define a model class for income
+class Income(Base):
+    __tablename__ = 'income'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Float)
+    item = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='income')
+
+    def __init__(self, amount, item, user):
+        self.amount = amount
+        self.item = item
+        self.user = user
+
+# define a model class for expenses
+class Expense(Base):
+    __tablename__ = 'expenses'
+
+    id = Column(Integer, primary_key=True)
+    amount = Column(Integer)
+    item = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='expenses')
+
+    def __init__(self, amount, item, user):
+        self.amount = amount
+        self.item = item
+        self.user = user
+
+
+if __name__ == "__main__":
+    # create the database tables
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session()
+
+    # create a new user
+    user = User(name='John Doe', age=30)
+
+    # add the user to the session
+    session.add(user)
+
+    # log an income
+    income = Income(amount=5000, item='Salary', user=user)
+    session.add(income)
+
+    # log an expense
+    expense = Expense(amount=1000, item='Rent', user=user)
+    session.add(expense)
+
+    # commit the transaction
+    session.commit()
+
+    # query the users table
+    users = session.query(User).all()
+
+    # print the users
+    for user in users:
+        print(user.name, user.age)
+        for income in user.income:
+            print('Income:', income.amount, income.item)
+        for expense in user.expenses:
+            print('Expense:', expense.amount, expense.item)
