210cbbeed257271f99ee2198be3dca0a5a5dc35f
Lalida Krairit <lala@Lalidas-MacBook-Pro-6.local>
2024-03-27 17:42:46 +0700

    first commit

---
+++ Fyn/fluttertest_application_1/android/app/src/main/kotlin/com/example/fluttertest_application_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttertest_application_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Fyn/fluttertest_application_1/lib/components/my_texfield.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          fillColor: Color.fromARGB(255, 243, 243, 245),
+          filled: true,
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            color: Color.fromRGBO(64, 100, 250, 0.6),
+          ),
+        ),
+      ),
+    );
+  }
+}
